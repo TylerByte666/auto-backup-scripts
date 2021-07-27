e9524e4a42b6d1cb62fda4d2e7f01b9442ccb299
Click Mac <clickmac@Clicks-Mac-mini.local>
2021-07-27 14:38:49 +0200

    Fixed wrong names

--- auto_backup_html.sh
+++ auto_backup_html.sh
@@ -1,50 +1,38 @@
 #!/bin/bash 
 ################################################################
 ##
-##   MySQL Database Backup Script 
+##   Web File Backup Script 
 ##   Written By: Tyler Geyser
 ##
 ################################################################ 
 TODAY=`date +"%d%b%Y"`
 ################################################################
 ################## Update below values  ######################## 
-DB_BACKUP_PATH='/var/backups'
-MYSQL_HOST='localhost'
-MYSQL_PORT='3306'
-MYSQL_USER=''
-MYSQL_PASSWORD=''
-DATABASE_NAME=''
+HTML_BACKUP_PATH='/var/backups'
+HTML_BACKUP_SRC='/var/www/html'
+APP_NAME=''
 BACKUP_RETAIN_DAYS=120   ## Number of days to keep local backup copy
-LOGFILE="${DB_BACKUP_PATH}/log/"db_backup_log_"$(date +'%Y_%m')".txt 
+LOGFILE="${HTML_BACKUP_PATH}/log/"html_backup_log_"$(date +'%Y_%m')".txt 
 #################################################################
- 
-mkdir -p ${DB_BACKUP_PATH}/${TODAY} 
-echo "Backup started for database - ${DATABASE_NAME} @ $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
+
+mkdir -p ${HTML_BACKUP_PATH}/${TODAY}
+echo "Backup started for html files @ $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
 echo "Backup retention: ${BACKUP_RETAIN_DAYS} days" >> "$LOGFILE"
- 
- 
-mysqldump -h ${MYSQL_HOST} \
-   -P ${MYSQL_PORT} \
-   -u ${MYSQL_USER} \
-   -p${MYSQL_PASSWORD} \
-   ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz
- 
-if [ $? -eq 0 ]; then
-  echo "Database backup successfully completed @ $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
-  echo "********************************************************************" >> "$LOGFILE"
-else
-  echo "Error found during backup @ $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
-  echo "********************************************************************" >> "$LOGFILE"
-  exit 1
-fi
- 
- 
+
+cd ${HTML_BACKUP_PATH}/${TODAY}
+tar -czf ${APP_NAME}-${TODAY}.tar.gz ${HTML_BACKUP_SRC}
+
+
+echo "HTML backup successfully completed @ $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
+echo "********************************************************************" >> "$LOGFILE"
+
+
 ##### Remove backups older than {BACKUP_RETAIN_DAYS} days  #####
  
 DBDELDATE=`date +"%d%b%Y" --date="${BACKUP_RETAIN_DAYS} days ago"`
  
-if [ ! -z ${DB_BACKUP_PATH} ]; then
-      cd ${DB_BACKUP_PATH}
+if [ ! -z ${HTML_BACKUP_PATH} ]; then
+      cd ${HTML_BACKUP_PATH}
       if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
             rm -rf ${DBDELDATE}
             echo "Deleted older backup: ${DBDELDATE} @ $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
